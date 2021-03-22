feba41358520ada75c856b5567d4f016c9a2a097
qqyurr <byyulli16@gmail.com>
2021-03-22 12:06:38 +0900

    [frontend] 기능추가 : 메인 페이지, full-page install

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './quasar'
+import VueFullPage from 'vue-fullpage.js'
 
 Vue.config.productionTip = false
+
+Vue.use(VueFullPage);
 
 new Vue({
   router,
---
+++ frontend/src/views/MainPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-page class="flex flex-center">
+    <full-page
+      ref="fullpage"
+      :options="options"
+      id="fullpage"
+      class="full-width"
+    >
+      <div class="full-width section">
+        <h1>section1</h1>
+      </div>
+      <div class="full-width section">
+        <h1>section1</h1>
+      </div>
+      <div class="full-width section">
+        <h1>section1</h1>
+      </div>
+    </full-page>
+    <q-page-sticky v-if="isScrolled" position="bottom-right" :offset="[25, 25]">
+      <q-btn
+        fab
+        icon="keyboard_arrow_up"
+        color="primary"
+        @click="$refs.fullpage.api.moveTo(1)"
+      />
+    </q-page-sticky>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  data() {
+    return {
+      options: {
+        navigation: true,
+        onLeave: this.onLeave,
+        licenseKey: "iTq67SN$c3"
+      },
+    };
+  },
+  methods: {
+    componentsReady(){
+      this.$refs.fullpage.init()
+    }
+  }
+}
+</script>
+
+<style>
+#section1 {
+  background-position: center;
+  background-size: unset;
+  background-image: url("../assets/monemone.jpg");
+}
+</style>
