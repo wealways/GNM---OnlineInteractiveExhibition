1c2da61c6b30b6fc657ed5a62230ff944f370e3e
dryans <dryans@naver.com>
2021-03-22 17:38:05 +0900

    feat : guestbook update, delete

--- backend/articles/views.py
+++ backend/articles/views.py
@@ -5,9 +5,11 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_http_methods
 from .models import Guestbook
 from rest_framework.decorators import api_view
+# from drf_yasg.utils import swagger_auto_schema
 
 # Create your views here.
 @api_view(['GET', 'POST'])
+# @swagger_auto_schema
 def article_list_create(request):
     if request.method == "GET":
         articles = Guestbook.objects.all()
@@ -21,3 +23,14 @@ def article_list_create(request):
             return JsonResponse(serializer.data)
     
 
+@api_view(['PUT', 'DELETE'])
+def article_update_delete(request,article_pk):
+    article = get_object_or_404(Guestbook, pk=article_pk)
+    if request.method == 'PUT':
+        serializer = GuestbookSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return JsonResponse(serializer.data)
+    else:
+        article.delete()
+        return JsonResponse({ 'id': article_pk })
