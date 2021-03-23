5965431197a04cf4994c6a78f7fb4db3be3b8efd
dryans <dryans@naver.com>
2021-03-24 00:07:30 +0900

    [backend] 기능추가 : 비밀번호 확인

--- backend/articles/urls.py
+++ backend/articles/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('', views.article_list_create),
-    path('<int:article_pk>', views.article_update_delete),
+    path('<int:article_pk>/', views.article_update_delete),
+    path('password/<int:article_pk>/', views.password_check),
+
 
 ]
--- backend/articles/views.py
+++ backend/articles/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_http_methods
 from .models import Guestbook
 from rest_framework.decorators import api_view
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 # from drf_yasg.utils import swagger_auto_schema
 
 # Create your views here.
@@ -27,13 +27,23 @@ def article_list_create(request):
 # @swagger_auto_schema(request_body=GuestbookBodySerializer)
 @api_view(['PUT', 'DELETE'])
 def article_update_delete(request,article_pk):  
-    # print(request.data.get('guestbook_password'))
+    # password = request.data.get('guestbook_password')
+    # article_password = get_object_or_404(Guestbook, pk=article_pk).guestbook_password
+    # res = check_password(password, article_password)
     article = get_object_or_404(Guestbook, pk=article_pk)
     if request.method == 'PUT':
         serializer = GuestbookSerializer(article, data=request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
+            guestbook_password = make_password(request.POST.get('guestbook_password'))
+            serializer.save(guestbook_password=guestbook_password)
             return JsonResponse(serializer.data)
     else:
         article.delete()
         return JsonResponse({ 'id': article_pk })
+
+@api_view(['POST'])
+def password_check(request, article_pk):
+    password = request.data.get('guestbook_password')
+    article_password = get_object_or_404(Guestbook, pk=article_pk).guestbook_password
+    res = check_password(password, article_password)
+    return JsonResponse({'result': res})
