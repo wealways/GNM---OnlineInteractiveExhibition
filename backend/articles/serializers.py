ac19fceb8f2329a34c31d70e3cbcda684cd3a90d
dryans1 <dryans@naver.com>
2021-03-23 10:39:18 +0900

    [backend] 기능추가 : 비밀번호 추가

--- backend/articles/serializers.py
+++ backend/articles/serializers.py
@@ -4,5 +4,10 @@ from .models import Guestbook
 class GuestbookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Guestbook
-        fields = ('user_nickname', 'guestbook_comment', 'guestbook_image', 'id', 'created_date', 'updated_date')
+        fields = ('user_nickname', 'guestbook_comment', 'guestbook_image', 'id', 'created_date', 'updated_date', 'guestbook_password')
         
+
+class GuestbookBodySerializer(serializers.Serializer):
+    user_nickname = serializers.CharField(help_text="user_nickname")
+    guestbook_comment = serializers.CharField(help_text="guestbook_comment")
+    guestbook_image = serializers.CharField(help_text="guestbook_image")
