c9cae36dcb166a096512e1f241f5b99e638c831b
qqyurr <byyulli16@gmail.com>
2021-04-01 12:39:45 +0900

    [frontend] 기능추가: scrollable but hidden

--- frontend/src/components/Mone/MoneInteractive.vue
+++ frontend/src/components/Mone/MoneInteractive.vue
@@ -107,6 +107,20 @@ export default {
 
 
 </style>
+<style>
+html {
+    overflow: scroll;
+    overflow-x: hidden;
+}
+::-webkit-scrollbar {
+    width: 0;  /* Remove scrollbar space */
+    background: transparent;  /* Optional: just make scrollbar invisible */
+}
+/* Optional: show position indicator in red */
+::-webkit-scrollbar-thumb {
+    background: #FF0000;
+}
+</style>
 <style lang="sass" scoped>
 html,body,#home,.ui-content
   margin: 0
