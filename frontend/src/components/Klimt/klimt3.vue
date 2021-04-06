01d50d0c2176238a44a2cdbab694913979f3d68b
wealways <likeletter5658@gmail.com>
2021-04-06 21:23:40 +0900

    [frontend] 디자인 : 클림트 리스트 디자인 완료

--- frontend/src/components/Klimt/klimt3.vue
+++ frontend/src/components/Klimt/klimt3.vue
@@ -6,11 +6,13 @@
       <img id="img31" class="img__none" src="@/assets/images/klimt/Gustav_Klimt_031.jpg" alt="더 트리 오브 라이프">
     </div>
     <div id="desc__thethree" class="description slide-in">
-      <h4>
-        기다림, 생명의 나무, 성취 <span style="font-size:1rem;">(1905 - 1909)</span>
+      <h4 class="desc-main__title">
+        Expectation,The three of life, fullfilment
       </h4>
-      Expectation,The three of life, fullfilment<br>
-      3개의 작품으로 나눠져 있으나 사실 하나의 작품입니다.나무의 사방으로 뻗어나간 가지들은 복잡하기 그지없는 삶의 면면들을 표현하고 있고 그 안에 꿈꾸듯 서 있는 두 연인은 삶을 초월하여 체념한 듯 서로에게 집착하며 서 있습니다.
+      <div class="desc-main__subtitle">기다림, 생명의 나무, 성취 <span style="font-size:1rem;">(1905 - 1909)</span></div>
+      <div class="desc-main__description">
+        3개의 작품으로 나눠져 있으나 사실 하나의 작품입니다.나무의 사방으로 뻗어나간 가지들은 복잡하기 그지없는 삶의 면면들을 표현하고 있고 그 안에 꿈꾸듯 서 있는 두 연인은 삶을 초월하여 체념한 듯 서로에게 집착하며 서 있습니다.
+      </div>
     </div>
   </div>
 </template>
@@ -55,6 +57,10 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Prata&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i');
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i');
+
 .section{
   padding: 3rem 5rem;
   color:aliceblue;
@@ -80,6 +86,20 @@ export default {
   flex-direction: column;
   align-items: center;
   padding: 0 20%;
+}
+.desc-main__title{
+  color:#FFD700;
+  font-family:'Playfair Display', serif;
+  letter-spacing: 3px;
+}
+.desc-main__subtitle{
+  font-family:'Open Sans', sans-serif;
+  font-size: 1.5rem;
+  margin:0.5rem 0;
+}
+.desc-main__description{
+  font-family:'Open Sans', sans-serif;
+  line-height: 22px;
 }
 .slide-in {
   opacity: 0;
