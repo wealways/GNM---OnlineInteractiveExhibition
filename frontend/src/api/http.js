79b1d7a1de0e11249b43c25558bca76cb6bdb5a6
wealways <likeletter5658@gmail.com>
2021-04-07 15:22:46 +0900

    [frontend] 기능추가 : 지도에서 메인페이지 갈 수 있게 하는 중

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'http://localhost:8000',
-  baseURL: 'http://j4c106.p.ssafy.io/api/',
+  baseURL: 'https://j4c106.p.ssafy.io/api/',
   headers:{
     // post:{
     //   'Access-Control-Allow-Origin':'*'
