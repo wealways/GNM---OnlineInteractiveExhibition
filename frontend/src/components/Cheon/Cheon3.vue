4ad18706835eb993075f97b80572c6352fb6346c
dryans <dryans@naver.com>
2021-04-07 19:48:20 +0900

    [frontend]기능추가 : 천명자 애니메이션 4번까지

--- frontend/src/components/Cheon/Cheon3.vue
+++ frontend/src/components/Cheon/Cheon3.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="cheon3">
       <img id="img" class="cheon3_pic slide-in" src="../../assets/kingGo.jpg" alt="고(孤)">
-      <div id="title" class="title"><h3>고(孤)</h3></div>
-      <div id="desc" class="description">
+      <div id="title" class="title slide-in"><h3>고(孤)</h3></div>
+      <div id="desc3" class="desc slide-in">
         이 작품은 과거의 문헌에 실렸던 사진을 보면 원래 화관과 나비가 지금과는 미묘하게 달랐습니다.
         후에 꽃의 수를 줄이고 디테일한 부분에서도 수정을 거친 작품입니다.
         천경자 작가의 완벽주의적 성향을 확인할 수 있습니다
-
       </div>
     </div>
 </template>
@@ -22,20 +21,20 @@ export default {
   watch:{
     'page':function(){
       const img = document.querySelector('#img')
-      const description = document.querySelector('#desc')
+      const desc = document.querySelector('#desc3')
       const title = document.querySelector('#title')
       if(this.page===3){
         setTimeout(function(){
           img.classList.add('active-image');
-        },500)
+        },600)
         setTimeout(function(){
+          desc.classList.add('active-desc');
           title.classList.add('active-title');
-          description.classList.add('active-description');
         },700)
-      }else{
-        img.classList.remove('active-img');
+      } else {
+        img.classList.remove('active-image');
         title.classList.remove('active-title');
-        description.classList.remove('active-description');
+        desc.classList.remove('active-desc');
       }
     }
   },
@@ -51,7 +50,7 @@ export default {
 
 .cheon3{
   position: relative;
-  background-color: #F2F2F2;
+  /* background-color: #F2F2F2; */
   z-index: -1;
   background-color: #eaeaea; 
 }
@@ -66,19 +65,6 @@ export default {
   top: 65%; 
   left: 47%; 
   text-shadow: 0 0 15px rgba(143, 143, 143, 0.658);
-}
-.description{
-  position: absolute;
-  top: 35%; 
-  font-size: 1rem;
-  left: 47%;
-
-}
-.title{
-  position: absolute;
-  top: 65%; 
-  left: 47%; 
-  text-shadow: 0 0 15px rgba(143, 143, 143, 0.658);
   opacity: 0;
 }
 
@@ -86,7 +72,7 @@ export default {
   font-family: 'Noto Serif KR', serif;
 }
 
-.description{
+.desc{
   position: absolute;
   top: 55%; 
   width: 40%;
@@ -96,23 +82,48 @@ export default {
   font-family: 'Open Sans', sans-serif;
   left: 47%;
   opacity: 0;
+  z-index: 10;
 }
 
-.active-image {
-  opacity: 1 !important;
-}
 
-.active-title {
-  opacity: 1 !important;
-}
 
-.active-description {
-  opacity: 1 !important;
-}
 
 .slide-in {
   opacity: 0;
   transition: all 1s;
 }
+
+
+
+
+#desc3.slide-in {
+  transform: translateY(-100%) scale(0.95);
+}
+
+.active-desc {
+  opacity: 1 !important;
+  transform: translateY(4%) scale(1) !important;
+}
+
+
+#title.slide-in {
+  transform: translateY(100%) scale(0.95);
+}
+
+.active-title {
+  opacity: 1 !important;
+  transform: translateY(4%) scale(1) !important;
+}
+
+
+.active-image {
+  opacity: 1 !important;
+  transform: translateX(0%) scale(1) !important;
+}
+
+#img.slide-in {
+  transform: translateX(-100%) scale(0.95);
+}
+
 
 </style>
