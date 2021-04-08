2c2e6b2cf70d95de0ff87a3a29f511231df16a05
wealways <likeletter5658@gmail.com>
2021-04-08 22:48:28 +0900

    [frontend] 기능추가 : 다운로드 기능

--- frontend/src/components/SpecialGallery/Output.vue
+++ frontend/src/components/SpecialGallery/Output.vue
@@ -17,7 +17,7 @@
       <div class="frame bigimage" id="bigimage">
         <img :src="changedImage" alt="img01">
       </div>
-      <div class="btn__save">
+      <div class="btn__save" @click="downloadImg">
         <q-btn class="btn" label="저장" size="10px"/>
       </div>
     </figure>
@@ -57,7 +57,43 @@ export default {
         bigframe.classList.add('big')
       })
     })
+  },
+  methods:{
+    dataURLtoBlob(dataurl) {
+      var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+      }
+      return new Blob([u8arr], {
+        type: mime
+      });
+    },
 
+    downloadImg() {
+      var image = new Image();
+      image.crossOrigin = "anonymous";
+      console.log(this.changedImage)
+      image.src = this.changedImage;
+      var fileName = image.src.split("/").pop();
+      image.onload = function() {
+        var canvas = document.createElement('canvas');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        canvas.getContext('2d').drawImage(this, 0, 0);
+        if (typeof window.navigator.msSaveBlob !== 'undefined') {
+          window.navigator.msSaveBlob(this.dataURLtoBlob(canvas.toDataURL()), fileName);
+        } else {
+          var link = document.createElement('a');
+          link.href = canvas.toDataURL();
+          link.download = fileName;
+          link.click();
+        }
+      };
+    }
   }
 }
 </script>
